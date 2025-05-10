b7fc28d66623af2fc4a82eb25376ee18ec58a03b
alejomu02 <134011820+alejomu02@users.noreply.github.com>
2025-05-10 19:24:09 +0000

    Creaccion del setup :white_check_mark:

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="edu_bigdata",
+    author="Alejandro Muñoz",
+    author_email="alejandro.munoz@est.iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "BeautifulSoup4"
+    ] 
+)
